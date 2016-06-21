cc5ccba91e64ffec527ca99f60393898f285bd3c
Mike Pack <mikepackdev@gmail.com>
2016-06-20 21:05:10 -0400

    Update actions behavior function signature

--- lib/wit_default_actions.ex
+++ lib/wit_default_actions.ex
@@ -7,14 +7,14 @@ defmodule Wit.DefaultActions do
   Called when the converse API has type `{type: "msg"}`.
   This should send message back to the user.
   """
-  @callback say(String.t, map, String.t) :: any
+  @callback say(String.t, map, String.t, map) :: any
 
   @doc """
   Called when the converse API has type `{type: "merge"}`.
   This should update the context with the information provided in the request
   """
-  @callback merge(String.t, map, map) :: map
+  @callback merge(String.t, map, map, map) :: map
 
 
-  @callback error(String.t, map, any) :: map
+  @callback error(String.t, map, any, map) :: map
 end
